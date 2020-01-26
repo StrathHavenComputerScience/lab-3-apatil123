5f21141a521a982a5cacc4f1a500795dbde535f1
apatil123 <51413901+apatil123@users.noreply.github.com>
2020-01-25 22:53:24 -0500

    Add files via upload

--- Lab3.java
+++ Lab3.java
@@ -17,9 +17,46 @@ public class Lab3
   public static void lightCandles()
   {
     //insert instructions below
-  
-  
-  
+    Robot.turnLeft(); Robot.move(); Robot.move(); turnRight(); Robot.move(); turnRight();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+    clearcolumn();
+  }
+  public static void clearcolumn()
+  {
+      //pre: Robot is facing down on a column that possibly has a candle.  
+      //post: Robot searches where to light the candle and ends up in the next column, facing down.
+      if (Robot.frontIsClear())
+      {
+          Robot.move(); if (Robot.frontIsClear()) {Robot.turnLeft(); Robot.turnLeft(); Robot.move(); turnRight(); if (Robot.frontIsClear()) {Robot.move(); turnRight();}} else {Robot.makeDark(); Robot.turnLeft(); Robot.turnLeft(); Robot.move(); turnRight(); if (Robot.frontIsClear()) {Robot.move(); turnRight();}}
+      }
+      else
+      {
+          Robot.makeDark(); Robot.turnLeft(); if (Robot.frontIsClear()) {Robot.move(); turnRight();} 
+      }
+  }
+  public static void turnRight()
+  { 
+    //pre: Robot is facing any direction
+    //post: Robot if facing to the right of the original position
+      Robot.turnLeft(); Robot.turnLeft(); Robot.turnLeft();
   }
   
   //Run this method to test completeRoom on map room1.txt
@@ -42,10 +79,49 @@ public class Lab3
   public static void completeRoom()
   {
     //insert instructions below
-  
-  
-  
+    checkleft();checkleft();checkleft();checkleft();checkleft();checkleft();
+    checkleft();checkleft();checkleft();checkleft();checkleft();checkleft();
+    checkleft();checkleft();checkleft();checkleft();checkleft();checkleft();
+    checkleft();checkleft();
   }
+  public static void checkleft()
+  {
+      //pre: Robot begins to the right of a wall or empty spot where a wall 
+      //pre: belongs.
+      //post: Robot ends in the next open spot, after filling in the wall.
+      if (Robot.frontIsClear()){
+      Robot.turnLeft();
+      functionone();
+    } 
+    else 
+    {
+    Robot.turnLeft();
+      if (Robot.frontIsClear()) {Robot.move(); if (Robot.onDark()) 
+          {Robot.turnLeft(); Robot.turnLeft();Robot.move();Robot.turnLeft();
+           if (Robot.frontIsClear()) {Robot.move();} else {turnRight();}} 
+          else {Robot.makeDark();Robot.turnLeft(); Robot.turnLeft();
+                Robot.move();Robot.turnLeft(); if (Robot.frontIsClear())
+                {Robot.move();} else {turnRight();}}}
+      else 
+      {
+          Robot.turnLeft();Robot.turnLeft();Robot.turnLeft(); 
+          if (Robot.frontIsClear()) {Robot.move();} else {turnRight();}
+      }
+    } 
+}
+public static void functionone()
+{if (Robot.frontIsClear()) {Robot.move(); if (Robot.onDark()) 
+          {Robot.turnLeft(); Robot.turnLeft();Robot.move();Robot.turnLeft();
+           if (Robot.frontIsClear()) {Robot.move();} else {turnRight();}} 
+          else {Robot.makeDark();Robot.turnLeft(); Robot.turnLeft();
+                Robot.move();Robot.turnLeft(); if (Robot.frontIsClear())
+                {Robot.move();} else {turnRight();}}}
+      else 
+      {
+          Robot.turnLeft();Robot.turnLeft();Robot.turnLeft(); 
+          if (Robot.frontIsClear()) {Robot.move();} else {turnRight();}
+      }
+}
   
   //Run this method to test swapAll on map swap1.txt
   public static void testSwapAll1()
@@ -67,9 +143,35 @@ public class Lab3
   public static void swapAll()
   {
     //insert instructions below
-  
-  
-  
+    rightp();Robot.move();rightp();Robot.move();rightp();Robot.move();rightp();Robot.move();rightp();
+    Robot.move();rightp();Robot.move();rightp();Robot.move();rightp();Robot.move();rightp();
+    Robot.move();rightp();
+    
+  }
+  public static void rightp()
+  {
+      turnRight();Robot.move(); if(Robot.onDark()) 
+      {Robot.makeLight();Robot.turnLeft();Robot.turnLeft();Robot.move();turnRight();rightr();}
+      else {Robot.turnLeft();Robot.turnLeft();Robot.move();turnRight();leftp();}
+  }
+  public static void leftp()
+  {
+      Robot.turnLeft();Robot.move(); if(Robot.onDark())
+      {Robot.makeLight();Robot.turnLeft();Robot.turnLeft();Robot.move();Robot.turnLeft();leftr();}
+      else {Robot.turnLeft();Robot.turnLeft();Robot.move();Robot.turnLeft();}
+  }
+  public static void rightr()
+  {
+      Robot.turnLeft();Robot.move(); if(Robot.onDark())
+      {Robot.turnLeft();Robot.turnLeft();Robot.move();Robot.turnLeft();turnRight();Robot.move();
+      Robot.makeDark();Robot.turnLeft();Robot.turnLeft();Robot.move();turnRight();}
+       else {Robot.makeDark();Robot.turnLeft();Robot.turnLeft();Robot.move();Robot.turnLeft();}
+  }
+  public static void leftr()
+  {
+      turnRight();Robot.move(); if(Robot.onDark()) 
+      {Robot.turnLeft();Robot.turnLeft();Robot.move();turnRight();}
+       else {Robot.makeDark();Robot.turnLeft();Robot.turnLeft();Robot.move();turnRight();}
   }
   
   //Don't run these. I will!
---
+++ RobotTest.java
@@ -0,0 +1,40 @@
+/*Feel free to run this class to check out your code. You will
+ * get an error message after six worlds. I have six secret worlds that I will be 
+ * using to test your code!*/
+
+public class RobotTest  {
+
+  public static void main(String[]args) {
+    
+    
+      Lab3.testLightCandles1();
+      Draw.pauseUntilMouse();
+      Lab3.testLightCandles2();
+      Draw.pauseUntilMouse();
+      /* Uncomment on Github before turning in
+      Lab3.testLightCandles3();
+      Draw.pauseUntilMouse();
+      Lab3.testLightCandles4();
+      Draw.pauseUntilMouse();*/
+      Lab3.testCompleteRoom1();
+      Draw.pauseUntilMouse();
+      Lab3.testCompleteRoom2();
+      Draw.pauseUntilMouse();
+      /* Uncomment on Github before turning in
+      Lab3.testCompleteRoom3();
+      Draw.pauseUntilMouse();
+      Lab3.testCompleteRoom4();
+      Draw.pauseUntilMouse();*/
+      Lab3.testSwapAll1();
+      Draw.pauseUntilMouse();
+      Lab3.testSwapAll2();
+      Draw.pauseUntilMouse();
+      /* Uncomment on Github before turning in
+      Lab3.testSwapAll3();
+      Draw.pauseUntilMouse();
+      Lab3.testSwapAll4();
+      Draw.pauseUntilMouse();*/    
+  
+
+}
+}
